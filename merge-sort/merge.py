ede9ced34d6057b2ed74d7491423b46df4994e02
sayeedk06 <sayeed.k06@gmail.com>
2020-06-16 01:40:31 +0600

    added merge-sort

--- merge-sort/merge.py
+++ merge-sort/merge.py
@@ -1,16 +1,55 @@
+A = []
 
-A = [1,6,7,8,4,20,44,30]
+"MERGE SORT"
 
-""" MERGE SORT """
+print("Enter a list of number to be sorted. Enter 0 to quit input and start sorting")
+x = 1
+while(x != "0"):
+    x = input("Enter a number = ")
+    A.append(int(x))
+    print(A)
+
+print("\n")
+
+def merge(left,right,sorted):
+    nL = len(left)
+    nR = len(right)
+
+    left_index = 0
+    right_index = 0
+    sort_index = 0
+
+    while(left_index < nL and right_index < nR):
+        if (left[left_index] < right[right_index]):
+            sorted[sort_index] = left[left_index]
+            sort_index += 1
+            left_index += 1
+        else:
+            sorted[sort_index] = right[right_index]
+            sort_index += 1
+            right_index += 1
+    while(left_index < nL):
+        sorted[sort_index] = left[left_index]
+        sort_index += 1
+        left_index += 1
+    while(right_index < nR):
+        sorted[sort_index] = right[right_index]
+        sort_index += 1
+        right_index += 1
+
+
+
+
+
 
 def merge_sort(A):
-    print(A)
+    # print(A)
     n = len(A)
     if(n<2):
         return A
     else:
         mid = int(n/2)
-        print("Mid = " + str(mid))
+        # print("Mid = " + str(mid))
         left = []
         right = []
         for i in range(0,mid):
@@ -18,14 +57,16 @@ def merge_sort(A):
             left.append(A[i])
         for i in range(mid,n):
             right.append(A[i])
-        print("Left = " + str(left))
-        print("right = " + str(right))
+        # print("Left = " + str(left))
+        # print("right = " + str(right))
         merge_sort(left)
         merge_sort(right)
+        merge(left, right,A)
 
 
 
 
 
-
+print("Unsorted = " + str(A))
 merge_sort(A)
+print("Sorted = " + str(A))
